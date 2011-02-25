fd3bbe7779e6b1d93088fcad02931b1746f9d7df
unknown <Venom@.(none)>
2011-02-25 15:59:47 -0500

    peters changes

--- c3dl/interaction/picking.js
+++ c3dl/interaction/picking.js
@@ -549,6 +549,7 @@ c3dl.rayIntersectsTriangle = function (orig, dir, vert0, vert1, vert2)
   // get the point of intersection, POI.
   var scaledDir = c3dl.multiplyVector(dir, t);
   var POI = c3dl.addVectors(orig, scaledDir);
+  
 
   // area of smaller triangles formed by the 3 vertices and the point of intersection	
   c3dl.subtractVectors(vert0, POI,edge1);
@@ -565,11 +566,27 @@ c3dl.rayIntersectsTriangle = function (orig, dir, vert0, vert1, vert2)
   // created where the user clicked. If the user clicked inside the triangle, the difference
   // should be near zero.
   var diff = area - (area1 + area2 + area3);
-
-  // delete edg1, edge2, edge3, area1, area2, area3, normDotDir, normDotRayorig, t, POI, area;
-  // since we have done quite a few calculations on floats, 
-  // allow a small margin of error.
-  return (Math.abs(diff) <= 0.0001);
+  if(Math.abs(diff) <= 0.0001) {
+    //get vector from ray origin to poi
+    var otherdir = c3dl.subtractVectors(POI,orig);
+    //get unit vector of that
+    var normOtherDir = c3dl.normalizeVector(otherdir);
+    
+    //get unit vector of original dir (uvd)
+    var normDir = c3dl.normalizeVector(dir);
+    //find the angle between those two vectors
+    var angle = c3dl.getAngleBetweenVectors(normOtherDir,normDir);
+    //if it is less than 90, the object is probably visible
+    if(angle < 90) {
+      return true;
+    }
+    else {//if it is greater than 90, this is behind the point of origin somewhere
+      return false;
+    } 
+  }
+  else {
+    return false;
+  }
 }
 
 c3dl.rayAABBIntersect = function (orig, dir, maxMins) {
